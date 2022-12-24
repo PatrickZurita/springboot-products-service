b34e0bf4636ba7b297ffa91569d15f50115666a5
Patrick Zurita <71539616+PatrickZurita@users.noreply.github.com>
2022-12-24 03:15:41 -0500

    add product model entity

---
+++ src/main/java/com/trickapp/springboot/app/products/models/entity/Product.java
@@ -0,0 +1,55 @@
+package com.trickapp.springboot.app.products.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="products")
+public class Product implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private Double price;
+
+	@Column(name = "create_at") // database nomenclature underscore_name 
+	@Temporal(TemporalType.DATE) // format
+	private Date createAt;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	private static final long serialVersionUID = 4290624447746106154L;
+}
