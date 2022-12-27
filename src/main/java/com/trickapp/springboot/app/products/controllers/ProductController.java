6a7178bb8b8d58c1394a5b806520538ddebff91d
Patrick Zurita <71539616+PatrickZurita@users.noreply.github.com>
2022-12-27 13:20:50 -0500

    desacoplando la url
    @Value("{server.port}")

--- src/main/java/com/trickapp/springboot/app/products/controllers/ProductController.java
+++ src/main/java/com/trickapp/springboot/app/products/controllers/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +15,22 @@ import com.trickapp.springboot.app.products.models.service.IProductService;
 
 @RestController
 public class ProductController {
-
+	
+	
 	@Autowired
 	private Environment env;
-
+	
+	@Value("${server.port}")
+	private Integer port;
+	
 	@Autowired
 	private IProductService productService;
 
 	@GetMapping("/getAll")
 	public List<Product> getAll() {
 		return productService.findAll().stream().map(product -> {
-			product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			product.setPort(port);
 			return product;
 		}).collect(Collectors.toList());
 	}
@@ -32,8 +38,8 @@ public class ProductController {
 	@GetMapping("/getById/{id}")
 	public Product getById(@PathVariable Long id) {
 		Product product = productService.findById(id);
-		product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-
+		//product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		product.setPort(port);
 		return product;
 	}
 }
