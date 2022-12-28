55d30b00121e2f242dc50043744287fb1e8fcaf1
Patrick Zurita <71539616+PatrickZurita@users.noreply.github.com>
2022-12-28 11:44:42 -0500

    update products service as eureka client

--- src/main/java/com/trickapp/springboot/app/products/SpringbootProductsServiceApplication.java
+++ src/main/java/com/trickapp/springboot/app/products/SpringbootProductsServiceApplication.java
@@ -2,8 +2,10 @@ package com.trickapp.springboot.app.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaClient 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.trickapp.springboot.app.products.models.service")
 @ComponentScan(basePackages = "com.trickapp.springboot.app.products.models.dao")
--- src/main/java/com/trickapp/springboot/app/products/controllers/ProductController.java
+++ src/main/java/com/trickapp/springboot/app/products/controllers/ProductController.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+//import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,21 +15,20 @@ import com.trickapp.springboot.app.products.models.service.IProductService;
 
 @RestController
 public class ProductController {
-	
-	
-	@Autowired
-	private Environment env;
-	
+
+	// @Autowired
+	// private Environment env;
+
 	@Value("${server.port}")
 	private Integer port;
-	
+
 	@Autowired
 	private IProductService productService;
 
 	@GetMapping("/getAll")
 	public List<Product> getAll() {
 		return productService.findAll().stream().map(product -> {
-			//product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			// product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 			product.setPort(port);
 			return product;
 		}).collect(Collectors.toList());
@@ -38,7 +37,7 @@ public class ProductController {
 	@GetMapping("/getById/{id}")
 	public Product getById(@PathVariable Long id) {
 		Product product = productService.findById(id);
-		//product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		// product.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		product.setPort(port);
 		return product;
 	}
